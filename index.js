ed6371a071443cc268bbb7702829e3d6d8deba2e
ramprasad.info@gmail.com <ram@smalldrops.nl>
2019-12-19 00:17:46 +0100

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req,res) => {
-    res.sendfile("index.html");
+    res.sendfile("index1.html");
 });
 
 app.listen(process.env.PORT || 5000, () => {
